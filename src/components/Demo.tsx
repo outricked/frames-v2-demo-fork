686ca300849f9f791e250eca9279cbf438890586
Rick <rick.huynh@coinbase.com>
2024-11-30 21:25:39 -0800

    open in app browser

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useCallback, useState, useMemo } from "react";
+import InAppBrowser from 'react-native-inappbrowser-reborn';
 import sdk, { type FrameContext } from "@farcaster/frame-sdk";
 import {
   useAccount,
@@ -79,8 +80,26 @@ export default function Demo() {
     );
   }, [linkReady, secureToken, chain?.name]);
 
-  const launch = useCallback(() => {
-    sdk.actions.openUrl(link);
+  const launch = useCallback(async () => {
+      try {
+        if (await InAppBrowser.isAvailable()) {
+          await InAppBrowser.open(link, {
+            // iOS Properties
+            dismissButtonStyle: 'cancel',
+            preferredBarTintColor: '#453AA4',
+            preferredControlTintColor: 'white',
+            readerMode: false,
+            // Android Properties
+            showTitle: true,
+            toolbarColor: '#6200EE',
+            secondaryToolbarColor: 'black',
+            enableUrlBarHiding: true,
+            enableDefaultShare: true,
+          });
+        }
+      } catch (error) {
+        console.error(error);
+      }
   }, [link]);
 
   // New handler function to call secureTokenWrapper and then launch
@@ -103,8 +122,6 @@ export default function Demo() {
 
       <div>
         <h2 className="font-2xl font-bold">Wallet</h2>
-
-      
         {address && (
           <div className="my-2 text-xs">
             <div>
