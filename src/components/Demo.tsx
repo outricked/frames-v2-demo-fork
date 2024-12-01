4a36dbe0a98d1921408881d52b7db6519d513b72
Rick <rick.huynh@coinbase.com>
2024-12-01 12:37:22 -0800

    baseline

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -2,24 +2,19 @@ import { useEffect, useCallback, useState } from "react";
 import sdk, { type FrameContext } from "@farcaster/frame-sdk";
 import {
   useAccount,
-  useDisconnect,
-  useConnect,
 } from "wagmi";
 
-import { config } from "~/components/providers/WagmiProvider";
 import { Button } from "~/components/ui/Button";
 import { truncateAddress } from "~/lib/truncateAddress";
 import { generateSecureToken } from "../utils/queries";
-import { Input } from "./ui/Input";
+import { Input } from "~/components/ui/Input";
 
 export default function Demo() {
   const [isSDKLoaded, setIsSDKLoaded] = useState(false);
   const [context, setContext] = useState<FrameContext>();
-  const [fiatValue, setFiatValue] = useState(5);
+  const [fiatValue, setFiatValue] = useState("");
   
-  const { address, chain, isConnected } = useAccount();
-  const { disconnect } = useDisconnect();
-  const { connect } = useConnect();
+  const { address, chain } = useAccount();
 
   useEffect(() => {
     const load = async () => {
@@ -67,7 +62,9 @@ export default function Demo() {
         : "") +
       `&fiatCurrency=USD` +
       `&presetFiatAmount=${fiatValue}` + 
-      `&redirectUrl=${encodeURIComponent("https://my-frames-v2-demo.vercel.app/")}`;
+      `&defaultAsset=USDC` +
+      `&redirectUrl=${encodeURIComponent("https://my-frames-v2-demo.vercel.app/")}` +
+      `&defaultPaymentMethod=APPLE_PAY`;
       console.log(constructedLink);
       window.location.href = constructedLink;
     } else {
@@ -81,9 +78,9 @@ export default function Demo() {
 
   return (
     <div className="w-[300px] mx-auto py-4 px-2">
-      <h1 className="text-2xl font-bold text-center mb-4">Frames v2 Demo</h1>
+      <h1 className="text-2xl font-bold text-center mb-4">Onramp Crypto To Farcaster Wallet</h1>
       <div>
-        <h2 className="font-2xl font-bold">Wallet</h2>
+        <h2 className="font-2xl font-bold">Connected Wallet</h2>
         {address && (
           <div className="my-2 text-xs">
             <div>
@@ -94,32 +91,35 @@ export default function Demo() {
             </div>
           </div>
         )}
-
-        <div className="mb-4">
-          <Button
-            onClick={() =>
-              isConnected
-                ? disconnect()
-                : connect({ connector: config.connectors[0] })
-            }
-          >
-            {isConnected ? "Disconnect" : "Connect"}
-          </Button>
-        </div>
       </div>
 
+      <br></br>
+      <div>
+      <h2 className="font-2xl font-bold">Enter USD amount</h2>
       <Input
-        label="Amount"
         id="amount"
-        type="number"
+        type="text"
         value={fiatValue}
-        onChange={(e) => setFiatValue(Number(e.target.value))}
-        placeholder="Enter amount of USD you want to buy"
+        onChange={(e) => setFiatValue(e.target.value)}
+        placeholder="5"
       />
-
+      
+      <br></br>
+      <h2 className="font-2xl font-bold">Enter Crypto Asset</h2>
+        <Input
+          id="amount"
+          type="text"
+          value={fiatValue}
+          onChange={(e) => setFiatValue(e.target.value)}
+          placeholder="5"
+        />
+      </div>
+      <br></br>
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+    </div>
       <div>
         <Button onClick={handleBuyCrypto}>
-          Buy {fiatValue} USD to address on {chain?.name}
+          Buy {fiatValue} USDC to {chain?.name}
         </Button>
       </div>
     </div>
